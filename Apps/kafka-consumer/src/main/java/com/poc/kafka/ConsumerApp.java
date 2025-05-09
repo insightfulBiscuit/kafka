a0d8b4b46a7db506703b3785dc4697fd088ea4ff
insightfulbiscuit@gmail.com <insightfulbiscuit@gmail.com>
2025-05-08 23:13:43 -0700

    Simple producer code written

---
+++ Apps/kafka-consumer/src/main/java/com/poc/kafka/ConsumerApp.java
@@ -0,0 +1,7 @@
+package com.poc.kafka;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        // Kafka consumer code here
+    }
+}
--- Apps/kafka-producer/src/main/java/com/poc/kafka/ProducerApp.java
+++ Apps/kafka-producer/src/main/java/com/poc/kafka/ProducerApp.java
@@ -1,7 +1,29 @@
 package com.poc.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;   // Kafka class to create a producer
+import org.apache.kafka.clients.producer.ProducerRecord;  // Kafka class to send a record
+import org.apache.kafka.clients.producer.ProducerConfig;  // Constants for producer configuration
+import org.apache.kafka.common.serialization.StringSerializer;  // Serializer for keys and values
+
+import java.util.Properties;
+
+
 public class ProducerApp {
     public static void main(String[] args) {
-        // Kafka producer code here
+        Properties props = new Properties();
+
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONGIF, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("topic", "key", "value");
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
     }
 }
