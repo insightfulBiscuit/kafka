873a3c359cb0876cb9d1393700186c7041f72462
insightfulbiscuit@gmail.com <insightfulbiscuit@gmail.com>
2025-05-08 23:30:45 -0700

    Fixed compilation errors of kafka producer code, configured maven compiler plugin to use new version of java

--- Apps/kafka-producer/src/main/java/com/poc/kafka/ProducerApp.java
+++ Apps/kafka-producer/src/main/java/com/poc/kafka/ProducerApp.java
@@ -12,13 +12,11 @@ public class ProducerApp {
     public static void main(String[] args) {
         Properties props = new Properties();
 
-        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONGIF, "localhost:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
 
         ProducerRecord<String, String> record = new ProducerRecord<>("topic", "key", "value");
         producer.send(record);
