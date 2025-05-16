3c00ec9959178ef7513ebc62c5f64150ffdc488c
insightfulbiscuit <insightfulbiscuit@gmail.com>
2025-05-15 21:01:27 -0700

    Wrote initial code for streaming

--- Apps/kafka-streams/src/main/java/com/poc/kafka/StreamApp.java
+++ Apps/kafka-streams/src/main/java/com/poc/kafka/StreamApp.java
@@ -1,13 +1,13 @@
 package com.poc.kafka;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.SteamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+public class StreamApp{
+    public static void main(String[] args){
+        
     }
 }
