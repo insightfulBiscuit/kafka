90b2a7370a953592c7ceb29a0982f0bb7d2c4b1d
insightfulbiscuit <insightfulbiscuit@gmail.com>
2025-05-20 17:37:14 -0700

    Edited shell scripts to update java path for kafka implementation on NVIDIA Jetson ORIN

--- terminal_implementation/create-topics.sh
+++ terminal_implementation/create-topics.sh
@@ -2,6 +2,9 @@
 set -e  # Exit on error
 set -x  # Print each command
 
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
+export PATH=$JAVA_HOME/bin:$PATH
+
 KAFKA_DIR="/opt/kafka"
 KAFKA_CLUSTER_ID="$($KAFKA_DIR/bin/kafka-storage.sh random-uuid)"
 BROKER="localhost:9092"
--- terminal_implementation/start-kafka-kraft.sh
+++ terminal_implementation/start-kafka-kraft.sh
@@ -2,6 +2,9 @@
 set -e  # Exit on error
 set -x  # Print each command
 
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64
+export PATH=$JAVA_HOME/bin:$PATH
+
 KAFKA_DIR="/opt/kafka"
 KAFKA_CLUSTER_ID="$($KAFKA_DIR/bin/kafka-storage.sh random-uuid)"
 BROKER="localhost:9092"
@@ -10,22 +13,6 @@ BROKER="localhost:9092"
 $KAFKA_DIR/bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c $KAFKA_DIR/config/server.properties
 
 $KAFKA_DIR/bin/kafka-server-start.sh $KAFKA_DIR/config/server.properties
-
-sleep 5
-
-$KAFKA_DIR/bin/kafka-topics.sh --create \
-  --bootstrap-server $BROKER \
-  --topic input-topic \
-  --partitions 1 \
-  --replication-factor 1 \
-  --if-not-exists
-
-$KAFKA_DIR/bin/kafka-topics.sh --create \
-  --bootstrap-server $BROKER \
-  --topic output-topic \
-  --partitions 1 \
-  --replication-factor 1 \
-  --if-not-exists
 
 # $KAFKA_DIR/bin/kafka-topics.sh --list --bootstrap-server $BROKER
 
